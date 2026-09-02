1d7e89989fcdaba28d0d18ef2737b45dc7b68c13
Pavel Astakhov <pastakhov@yandex.ru>
2026-09-02 13:32:58 +0500

    Show the retained article field on the 1.43 delete form

--- includes/OverwrittenDeleteAction.php
+++ includes/OverwrittenDeleteAction.php
@@ -3,69 +3,32 @@
 namespace MediaWiki\Extension\RetainedArticles;
 
 use DeleteAction;
-use HTMLForm;
-use HTMLTitleTextField;
 
 class OverwrittenDeleteAction extends DeleteAction {
 
 	/**
 	 * @inheritDoc
 	 */
-	protected function showForm( string $reason ): void {
-		parent::showForm( $reason );
-		$offset = $this->getOffsetOfSubmitButtonFieldLayout();
-		if ( $offset ) {
-			$outputPage = $this->getOutput();
+	protected function getFormFields(): array {
+		$fields = parent::getFormFields();
+		$retainedArticle = [
+			'type' => 'title',
+			'label-message' => 'retained-articles-title-label',
+			'name' => 'retained-article',
+			'required' => false,
+		];
 
-			$form = HTMLForm::factory( 'ooui', [], $this->getContext() );
-			$field = HTMLForm::loadInputFromParameters(
-				'retainedArticle',
-				[
-					'class' => HTMLTitleTextField::class,
-					'label-message' => 'retained-articles-title-label',
-					'name' => 'retained-article',
-					'required' => false,
-				],
-				$form
-			);
-			$fieldHtml = $field->getOOUI( '' );
-			$outputBodyText = $outputPage->mBodytext;
-			// Adds required modules
-			$form->prepareForm()->displayForm( false );
-			$outputPage->mBodytext = substr_replace( $outputBodyText, $fieldHtml, $offset, 0 );
-		}
-	}
-
-	/**
-	 * @param string $string
-	 * @param int|null $offset
-	 * @return bool
-	 */
-	private static function hasSubmitButton( string $string, ?int $offset = 0 ): bool {
-		$pattern = <<<EOD
-/<button[^>]*type=['"]submit['"]/
-EOD;
-		return preg_match( $pattern, $string, $m, 0, $offset ) !== false;
-	}
-
-	/**
-	 * @return int|null
-	 */
-	private function getOffsetOfSubmitButtonFieldLayout(): ?int {
-		$outputPage = $this->getOutput();
-		$outputHtml = $outputPage->mBodytext;
-		$pattern = <<<EOD
-/<div[^>]*class=['"][^'"]*oo-ui-fieldLayout[ '"]/
-EOD;
-		preg_match_all( $pattern, $outputHtml, $matches, PREG_OFFSET_CAPTURE );
-		$offset = null;
-		while ( $matches && $matches[0] ) {
-			$m = array_pop( $matches[0] );
-			if ( self::hasSubmitButton( $outputHtml, $m[1] ) ) {
-				$offset = $m[1];
-				break;
+		$result = [];
+		foreach ( $fields as $key => $field ) {
+			if ( $key === 'ConfirmB' ) {
+				$result['RetainedArticle'] = $retainedArticle;
 			}
+			$result[$key] = $field;
 		}
-		return $offset;
+		if ( !isset( $result['RetainedArticle'] ) ) {
+			$result['RetainedArticle'] = $retainedArticle;
+		}
+
+		return $result;
 	}
 }
